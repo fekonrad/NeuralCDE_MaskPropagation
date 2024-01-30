ffdd41bb97e98a14ac576753b150c61fd7513131
Felix Konrad <110739309+fekonrad@users.noreply.github.com>
2024-01-30 12:50:31 +0100

    Add files via upload

--- Models/CT_UNet.py
+++ Models/CT_UNet.py
@@ -19,27 +19,31 @@ class ConvCDEFunc(nn.Module):
 
 
 class CT_UNet(nn.Module):
-    def __init__(self, num_channels=1):
+    def __init__(self, num_channels=1, hidden_channels=32, atol=1e-4, rtol=1e-3, method=None):
         super().__init__()
+        self.hidden_channels = hidden_channels
+        self.atol = atol
+        self.rtol = rtol
+        self.method = method
 
         # encoder
-        self.down1 = self.conv_block(num_channels, 64)
-        self.down2 = self.conv_block(64, 128)
-        self.down3 = self.conv_block(128, 256)
-        self.down4 = self.conv_block(256, 512)
+        self.down1 = self.conv_block(num_channels, hidden_channels)
+        self.down2 = self.conv_block(hidden_channels, 2 * hidden_channels)
+        self.down3 = self.conv_block(2 * hidden_channels, 4 * hidden_channels)
+        self.down4 = self.conv_block(4 * hidden_channels, 8 * hidden_channels)
 
         # neural CDE Func
-        self.cde_func = ConvCDEFunc(in_channels=512, hidden_channels=1024)
+        self.cde_func = ConvCDEFunc(in_channels=8 * hidden_channels, hidden_channels=16 * hidden_channels)
 
         # decoder
-        self.up1 = self.conv_block(1024, 512)
-        self.conv_trans1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.up2 = self.conv_block(512, 256)
-        self.conv_trans2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.up3 = self.conv_block(256, 128)
-        self.conv_trans3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.up4 = self.conv_block(128, 64)
-        self.readout = nn.Conv2d(64, 1, kernel_size=1, padding="same")
+        self.up1 = self.conv_block(16 * hidden_channels, 8 * hidden_channels)
+        self.conv_trans1 = nn.ConvTranspose2d(8 * hidden_channels, 4 * hidden_channels, kernel_size=2, stride=2)
+        self.up2 = self.conv_block(8 * hidden_channels, 4 * hidden_channels)
+        self.conv_trans2 = nn.ConvTranspose2d(4 * hidden_channels, 2 * hidden_channels, kernel_size=2, stride=2)
+        self.up3 = self.conv_block(4 * hidden_channels, 2 * hidden_channels)
+        self.conv_trans3 = nn.ConvTranspose2d(2 * hidden_channels, hidden_channels, kernel_size=2, stride=2)
+        self.up4 = self.conv_block(2 * hidden_channels, hidden_channels)
+        self.readout = nn.Conv2d(hidden_channels, 1, kernel_size=1, padding="same")
 
         # mask encoder and decoder
         self.mask_encoder = self.__create_mask_encoder__()
@@ -54,13 +58,13 @@ class CT_UNet(nn.Module):
 
     def __create_mask_encoder__(self):
         encoder = nn.Sequential(
-            self.conv_block(1, 64),
+            self.conv_block(1, self.hidden_channels),
             nn.MaxPool2d(2),
-            self.conv_block(64, 128),
+            self.conv_block(self.hidden_channels, 2 * self.hidden_channels),
             nn.MaxPool2d(2),
-            self.conv_block(128, 256),
+            self.conv_block(2 * self.hidden_channels, 4 * self.hidden_channels),
             nn.MaxPool2d(2),
-            self.conv_block(256, 512),
+            self.conv_block(4 * self.hidden_channels, 8 * self.hidden_channels),
             nn.MaxPool2d(2),
         )
         return encoder
@@ -79,14 +83,14 @@ class CT_UNet(nn.Module):
         z0 = self.mask_encoder(y0)
 
         x = x.view(-1, c, h, w)  # reshape tensor to make it compatible with Conv2d layers
-        res1 = self.down1(x)  
-        x = nn.MaxPool2d(2)(res1)  
-        res2 = self.down2(x)  
-        x = nn.MaxPool2d(2)(res2) 
-        res3 = self.down3(x)  
-        x = nn.MaxPool2d(2)(res3)  
-        res4 = self.down4(x)  
-        x = nn.MaxPool2d(2)(res4)  
+        res1 = self.down1(x)
+        x = nn.MaxPool2d(2)(res1)
+        res2 = self.down2(x)
+        x = nn.MaxPool2d(2)(res2)
+        res3 = self.down3(x)
+        x = nn.MaxPool2d(2)(res3)
+        res4 = self.down4(x)
+        x = nn.MaxPool2d(2)(res4)
 
         # Step 2: Compute Spline and Solve CDE in the Latent Space at specified time points
         _, latent_c, latent_h, latent_w = x.shape
@@ -96,15 +100,15 @@ class CT_UNet(nn.Module):
         for index, video in enumerate(x):
             t = times[index]
             spline = CubicSplineInt(t, video)
-            z[index] = cde_solve(spline, self.cde_func, z0[index], t)
+            z[index] = cde_solve(spline, self.cde_func, z0[index], t, atol=self.atol, rtol=self.rtol, method=self.method)
 
         # Step 3: Decode CDE Solution back into the ambient space, add residuals
         x = z.view(-1, latent_c, latent_h, latent_w)  # need to reshape again for Conv2d
 
-        x = nn.Upsample(scale_factor=2)(x) 
-        x = self.up1(torch.cat([x, res4], dim=1))  
-        x = self.conv_trans1(x)  
-        x = self.up2(torch.cat([x, res3], dim=1)) 
+        x = nn.Upsample(scale_factor=2)(x)
+        x = self.up1(torch.cat([x, res4], dim=1))
+        x = self.conv_trans1(x)
+        x = self.up2(torch.cat([x, res3], dim=1))
         x = self.conv_trans2(x)
         x = self.up3(torch.cat([x, res2], dim=1))
         x = self.conv_trans3(x)
